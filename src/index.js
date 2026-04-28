21f11eb0ea689d47b851585857d009a83dbec0be
Naijastudy <ayomidepaul986@gmail.com>
2026-04-28 16:26:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkeregistration from './serviceWorkeregistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();

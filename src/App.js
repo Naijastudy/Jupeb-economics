99406874bd13413a6b4099eb0112ee4123c7a2aa
Naijastudy <ayomidepaul986@gmail.com>
2026-04-30 11:30:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function App() {
-  return <h1>Finally working!</h1>;
+  return <h1 style={{color: 'white', textAlign: 'center', marginTop: '50px'}}>Success!</h1>;
 }

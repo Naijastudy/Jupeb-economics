39b9d5a72eece5e7861ded6d1b7691652fba317f
Naijastudy <ayomidepaul986@gmail.com>
2026-05-06 23:04:26 +0100

    Update Settings.js

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React  from "react";
+import Header from "./components/Header";
+import Feedback from "./screens/Feedback";
 
 export default function Settings({
   t,

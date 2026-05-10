9c342f66f57d3891a3f06749941b85b79fb2ef32
Naijastudy <ayomidepaul986@gmail.com>
2026-05-10 14:42:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
 
 serviceWorkerRegistration.register();

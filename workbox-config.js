9b8709d5f22d6656f40ae178ae40c12fc34c94bf
Naijastudy <ayomidepaul986@gmail.com>
2026-05-11 11:51:53 +0100

    Delete workbox-config.js

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  globDirectory: "build/",
-  globPatterns: ["**/*.{js,css,html,png,jpg,svg,ico,json}"],
-  swDest: "build/service-worker.js",
-  swSrc: "public/service-worker.js",
-  maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-};

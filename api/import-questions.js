002a20bd1f0551ed7649c4189c20c5cc2c5ac252
Naijastudy <ayomidepaul986@gmail.com>
2026-05-13 20:51:06 +0100

    Update import-questions.js

--- api/import-questions.js
+++ api/import-questions.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  if (req.query.secret !== process.env.IMPORT_SECRET) {
+module.exports = async function handler(req, res) {  if (req.query.secret !== process.env.IMPORT_SECRET) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 

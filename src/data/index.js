95a3478d32ca5870aadda239032557525226aa96
Naijastudy <ayomidepaul986@gmail.com>
2026-05-15 17:36:39 +0100

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,38 +1,39 @@
 import * as economics from "./economics";
 import * as government from "./government/index";
 import * as business_studies from "./business_studies/index";
+import * as accounting from "./accounting/index";
 
 export const subjects = [
-  {
-    id: "economics",
-    name: "Economics",
-    emoji: "📊",
-    color: "#6b95f1",
-    available: true,
-    data: economics,
-  },
-  {
-    id: "government",
-    name: "Government",
-    emoji: "🏛️",
-    color: "#1d4ed8",
-    available: true,
-    data: government,
-  },
-  {
-    id: "business_studies",
-    name: "Business studies",
-    emoji: "💼",
-    color: "#6b95f1",
-    available: false,
-    data: business_studies,
-  },
-   {
-    id: "accounting",
-    name: "Accounting",
-    emoji: "💼",
-    color: "#6b95f1",
-    available: false,
-    data: accounting,
-  },
+{
+  id: "economics",
+  name: "Economics",
+  emoji: "📈",
+  color: "#2563EB",
+  available: true,
+  data: economics,
+},
+{
+  id: "government",
+  name: "Government",
+  emoji: "🏛️",
+  color: "#1E3A8A", 
+  available: true,
+  data: government,
+},
+{
+  id: "business_studies",
+  name: "Business studies",
+  emoji: "💼",
+  color: "#7C3AED",
+  available: false,
+  data: business_studies,
+},
+{
+  id: "accounting",
+  name: "Accounting",
+  emoji: "🏢",
+  color: "#059669",
+  available: false,
+  data: accounting,
+},
 ];

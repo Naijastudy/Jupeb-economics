e6c999b1a27a53915d2a81177af3e224833914c4
Naijastudy <ayomidepaul986@gmail.com>
2026-05-16 00:51:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import useNotifications from "./hooks/useNotifications";
 import NotificationSettings from "./screens/NotificationSettings";
 import useToast from "./hooks/useToast";
 import Toast from "./components/Toast";
-import useFCM from "./hooks/useFCM";
+/*import useFCM from "./hooks/useFCM";*/
 // ── SHARED UTILITY ────────────────────────────────────────────────────────────
 function formatTime(seconds) {
   const h = Math.floor(seconds / 3600);

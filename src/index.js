aebdd86c27589e1d3c3ae2b744be41a97c860e1a
Naijastudy <ayomidepaul986@gmail.com>
2026-05-18 15:23:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,28 +24,6 @@ root.render(
     </AppProvider>
   </ErrorBoundary>
 );
-/*import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
-import { AppProvider } from "./context/AppContext";
-import GlobalStyles from "./components/GlobalStyles";
-import * as serviceWorkerRegistration
-  from "./serviceWorkerRegistration";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-
-root.render(
-  <ErrorBoundary>
-    <AppProvider>
-      <GlobalStyles />
-      <App />
-    </AppProvider>
-  </ErrorBoundary>
-);
-*/
 // Register service worker for offline support
 serviceWorkerRegistration.register({
   onSuccess: () => {
